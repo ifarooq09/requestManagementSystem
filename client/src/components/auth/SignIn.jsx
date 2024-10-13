66b301e89b298300483ee4a733aedf30ec19efcb
Ibrahim <ifarooq9@gmail.com>
2024-10-13 10:16:39 +0430

    minor changes

--- client/src/components/auth/SignIn.jsx
+++ client/src/components/auth/SignIn.jsx
@@ -1,27 +1,32 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { Alert } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Alert } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiService } from '../services/apiService.js';
-import { LOGIN_ROUTE } from '../services/constants';
+import { apiService } from "../services/apiService.js";
+import { LOGIN_ROUTE } from "../services/constants";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://www.mail.gov.af">
         IT Directorate - Ministry of Agriculture, Irrigation and Livestock
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -38,9 +43,9 @@ const defaultTheme = createTheme({
 export default function SignIn() {
   const [loginVal, setLoginVal] = useState({
     email: "",
-    password: ""
+    password: "",
   });
-  const [errorMsg, setErrorMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState("");
 
   const navigate = useNavigate();
 
@@ -67,11 +72,8 @@ export default function SignIn() {
       setErrorMsg("Password must be of at least 8 characters");
     } else {
       try {
-        const response = await apiService.post(
-            LOGIN_ROUTE,
-            loginVal
-        )
-        console.log(response)
+        const response = await apiService.post(LOGIN_ROUTE, loginVal);
+        console.log(response);
 
         if (response.status === 200) {
           localStorage.setItem("usersdatatoken", response.data.result.token);
@@ -79,7 +81,9 @@ export default function SignIn() {
           setLoginVal({ email: "", password: "" });
           setErrorMsg(""); // Clear error message on successful login
         } else if (response.status === 423) {
-          setErrorMsg("This account has been suspended! Try to contact the admin");
+          setErrorMsg(
+            "This account has been suspended! Try to contact the admin"
+          );
         } else {
           setErrorMsg("Login failed. Please try again.");
         }
@@ -97,34 +101,62 @@ export default function SignIn() {
           {errorMsg}
         </Alert>
       )}
-      <Container component="main" maxWidth="xs" sx={{ backgroundColor: '#f2f3f4', borderRadius: 3, border: '2px solid green', marginTop: 10, marginBottom: 5 }}>
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          backgroundColor: "#f2f3f4",
+          borderRadius: 3,
+          border: "2px solid green",
+          marginTop: 10,
+          marginBottom: 5,
+        }}
+      >
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            marginTop: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-            <Typography component="h1" variant="h5" sx={{
-                marginBottom: "10px",
-                fontFamily: "monospace"
-            }}>
-            سیستم ثبت پیشنهاد و حکم مقام ریاست دفتر
-            </Typography>
           <Avatar
             alt="Ministry of Agriculture, Irrigation and Livestock"
             src="/MAILLogoTransparent.png"
             sx={{ width: 180, height: 180, marginBottom: 2 }}
           />
-          <Typography component="h1" variant="h5" sx={{
-            textAlign: "right",
-            fontSize: "18px",
-            fontWeight: "bold",
-            fontFamily:"-moz-initial"
-          }}>
-          یوزر کاربری و رمز عبور را وارد کنید تا وارد سیستم شوید
+          <Typography
+            component="h1"
+            variant="h3"
+            sx={{
+              marginBottom: "10px",
+              fontFamily: "monospace",
+            }}
+          >
+           ریاست دفتر
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{
+              marginBottom: "10px",
+              fontFamily: "monospace",
+            }}
+          >
+            سیستم ثبت پیشنهاد و حکم مقام
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h6"
+            sx={{
+              textAlign: "right",
+              fontSize: "18px",
+              fontWeight: "bold",
+              fontFamily: "-moz-initial",
+            }}
+          >
+            یوزر کاربری و رمز عبور را وارد کنید تا وارد سیستم شوید
           </Typography>
           <Box component="form" onSubmit={loginUser} noValidate sx={{ mt: 1 }}>
             <TextField
@@ -140,14 +172,14 @@ export default function SignIn() {
               autoFocus
               onChange={loginValue}
               sx={{
-                '& label': {
-                  right: 0, 
-                  transformOrigin: 'top', 
+                "& label": {
+                  right: 0,
+                  transformOrigin: "top",
                 },
-                '& label.Mui-focused': {
-                  right: 0, 
+                "& label.Mui-focused": {
+                  right: 0,
                 },
-                textAlign: 'right', 
+                textAlign: "right",
               }}
             />
             <TextField
@@ -163,14 +195,14 @@ export default function SignIn() {
               autoComplete="current-password"
               onChange={loginValue}
               sx={{
-                '& label': {
-                  right: 0, 
-                  transformOrigin: 'top', 
+                "& label": {
+                  right: 0,
+                  transformOrigin: "top",
                 },
-                '& label.Mui-focused': {
-                  right: 0, 
+                "& label.Mui-focused": {
+                  right: 0,
                 },
-                textAlign: 'right', 
+                textAlign: "right",
               }}
             />
             <Button
